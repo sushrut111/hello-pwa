30a0ba24b70b9c20d97d0d72363ef3d7d41cb57d
Sushrut Kasture <kasturesushrut@gmail.com>
2021-02-28 14:55:19 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,7 +11,22 @@ window.onload = () => {
   function displayNotification() {
     if (Notification.permission == 'granted') {
       navigator.serviceWorker.getRegistration().then(function(reg) {
-        reg.showNotification('Hello world!');
+        var options = {
+          body: 'Here is a notification body!',
+          icon: 'images/example.png',
+          vibrate: [100, 50, 100],
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+          },
+          actions: [
+            {action: 'explore', title: 'Explore this new world',
+              icon: 'images/checkmark.png'},
+            {action: 'close', title: 'Close notification',
+              icon: 'images/xmark.png'},
+          ]
+        };
+        reg.showNotification('Hello world!', options);
       });
     }
   }
