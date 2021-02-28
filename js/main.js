f6ef5f0aa71640e6a8d80bac5f0c38978f9f187d
Sushrut Kasture <kasturesushrut@gmail.com>
2021-02-28 14:39:45 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,4 +8,12 @@ window.onload = () => {
   Notification.requestPermission(function(status) {
     alert('Notification permission status:', status);
   });
+  function displayNotification() {
+    if (Notification.permission == 'granted') {
+      navigator.serviceWorker.getRegistration().then(function(reg) {
+        reg.showNotification('Hello world!');
+      });
+    }
+  }
+  displayNotification();
 }
