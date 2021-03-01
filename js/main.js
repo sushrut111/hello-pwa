9381b1b899af986d04d19496ddc452c62a297a0b
Sushrut Kasture <kasturesushrut@gmail.com>
2021-03-01 14:54:37 +0530

    backend integ

--- js/main.js
+++ js/main.js
@@ -49,21 +49,21 @@ function subscribeUser() {
 subscribeUser();
 
 const addSubscription = () => {
-  var data = "subscription=%7B%22endpoint%22%3A%22https%3A%2F%2Ffcm.googleapis.com%2Ffcm%2Fsend%2Fd2IfkmjUW38%3AAPA91bGdJuzk6cQBE0YRILMifCEcLyvApLfGtgYNBC0pGHmC7k0GOWMSRO24TRrgkUtHbJN1_CXaFs2fw9DTdrXmDQX2hV3PlJIZ53ISQgfVcB1q2UzXONDiZ59gSKW4Ucy1Dbz927Rz%22%2C%22expirationTime%22%3Anull%2C%22keys%22%3A%7B%22p256dh%22%3A%22BAN40sfVxDonCW_faEfx46SO-fHCysUI5ogj_-gLzZkjOKccrR6TEmz5UkwZ5T3K-8i2F1gPNulbF0uSEK4TlDY%22%2C%22auth%22%3A%228h_5ejS0mB1DMjrUgN4BlQ%22%7D%7D";
+  var data = "subscription=%7B%22endpoint%22%3A%22https%3A%2F%2Ffcm.googleapis.com%2Ffcm%2Fsend%2Ff6xbkT3te48%3AAPA91bGtbTHgfGwC_hWXWREqcd4nV5sZircVQ21KQnSjpONw3sCPFuvwSw0EG1sWDLQMi6ynvMlhq7okUJhQomwYoad2-Qu2Uko2jxv4yTcoRm21xOXH5s0ApKHX4_Ls6fyA9N8Cp27v%22%2C%22expirationTime%22%3Anull%2C%22keys%22%3A%7B%22p256dh%22%3A%22BI5SmCWibgxAWE79uC-m2Suw29nDjeiLtEUr9_xuRk3SQMTP5DdrdZGUPndXceYiN_ez0cuqI3EJGvNSgY8UKXg%22%2C%22auth%22%3A%22ulN0rU2xMM0xgajeN2YypQ%22%7D%7D";
 
   var xhr = new XMLHttpRequest();
   xhr.withCredentials = true;
-
+  
   xhr.addEventListener("readystatechange", function () {
     if (this.readyState === 4) {
       console.log(this.responseText);
     }
   });
-
+  
   xhr.open("POST", "https://5h4wsq.deta.dev/addsubscription");
   xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
   xhr.setRequestHeader("cache-control", "no-cache");
-  xhr.setRequestHeader("postman-token", "039c67af-f7d2-c4f9-176c-2de26470a894");
-
+  xhr.setRequestHeader("postman-token", "726f0e73-2c04-e301-bb7c-216233e5cace");
+  
   xhr.send(data);
 }
