c057ff9fd41991246620c6a2c11f4caf8bfa10bb
Sushrut Kasture <kasturesushrut@gmail.com>
2021-03-01 14:42:26 +0530

    backend integ

--- js/main.js
+++ js/main.js
@@ -11,7 +11,8 @@ window.onload = () => {
           console.log('Not subscribed to push service!');
         } else {
           // We have a subscription, update the database
-          console.log('Subscription object: ', sub);
+          console.log('Subscription object: ', JSON.stringify(sub));
+          addSubscription();
         }
       });
     })
@@ -25,7 +26,6 @@ window.onload = () => {
  
   
 }
-
 function subscribeUser() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(function(reg) {
@@ -34,6 +34,7 @@ function subscribeUser() {
         applicationServerKey: "BITeUzZcUPoOJ2RXPRPvekF7j0gpb3Ausx7qBTkobn1CktxKQfU2kr_zoor518ubFhzkxMfTzdYcwjHDp_VgkB8"
       }).then(function(sub) {
         console.log(JSON.stringify(sub));
+        addSubscription();
       }).catch(function(e) {
         if (Notification.permission === 'denied') {
           console.warn('Permission for notifications was denied');
@@ -47,3 +48,22 @@ function subscribeUser() {
 }
 subscribeUser();
 
+const addSubscription = () => {
+  var data = "subscription=%7B%22endpoint%22%3A%22https%3A%2F%2Ffcm.googleapis.com%2Ffcm%2Fsend%2Fd2IfkmjUW38%3AAPA91bGdJuzk6cQBE0YRILMifCEcLyvApLfGtgYNBC0pGHmC7k0GOWMSRO24TRrgkUtHbJN1_CXaFs2fw9DTdrXmDQX2hV3PlJIZ53ISQgfVcB1q2UzXONDiZ59gSKW4Ucy1Dbz927Rz%22%2C%22expirationTime%22%3Anull%2C%22keys%22%3A%7B%22p256dh%22%3A%22BAN40sfVxDonCW_faEfx46SO-fHCysUI5ogj_-gLzZkjOKccrR6TEmz5UkwZ5T3K-8i2F1gPNulbF0uSEK4TlDY%22%2C%22auth%22%3A%228h_5ejS0mB1DMjrUgN4BlQ%22%7D%7D";
+
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open("POST", "https://5h4wsq.deta.dev/addsubscription");
+  xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+  xhr.setRequestHeader("cache-control", "no-cache");
+  xhr.setRequestHeader("postman-token", "039c67af-f7d2-c4f9-176c-2de26470a894");
+
+  xhr.send(data);
+}
